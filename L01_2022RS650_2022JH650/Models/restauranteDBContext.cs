1d02989ae8f6c9a32e91122e3ccd0cc1f504cc7d
Eon-ts <aeonsitux@gmail.com>
2025-02-20 17:16:18 -0600

    Agregar archivos de proyecto.

---
+++ L01_2022RS650_2022JH650/Models/restauranteDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace L01_2022RS650_2022JH650.Models
+{
+    public class restauranteDBContext : DbContext
+    {
+        public restauranteDBContext(DbContextOptions<restauranteDBContext> options) : base(options)
+        {
+        }
+
+    }
+}
